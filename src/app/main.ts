2fd941f7eecf82ef4a143e02b7a054b1b827ce7d
garethweaver <reg@garethweaver.com>
2017-03-25 09:03:38 +0000

    Create angular app modules and compile with babel, browserfiy and tsify

---
+++ src/app/main.ts
@@ -0,0 +1,6 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { BrowserModule } from '@angular/platform-browser'
+import { AppModule } from './app.module'
+
+const platform = platformBrowserDynamic()
+platform.bootstrapModule(AppModule)
